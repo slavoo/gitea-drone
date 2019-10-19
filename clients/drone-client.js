0a4d5ba8d5f5b5b1596273da42ae7979389ad1e7
slavoo <4722492+slavoo@users.noreply.github.com>
2019-10-19 18:48:11 +0100

    added logs and better error detection

--- clients/drone-client.js
+++ clients/drone-client.js
@@ -5,15 +5,30 @@ const { DRONE_URL, DRONE_TOKEN } = require('../config')
 function refreshRepos(callback, errorCallback) {
     request.post(url.resolve(DRONE_URL, `/api/user/repos`))
         .auth(bearer = DRONE_TOKEN)
-        .on('response', (r1) => { callback(); })
+        .on('response', resp => {
+            successOrFaiulre(resp, callback, errorCallback);
+        })
         .on('error', (err) => { errorCallback(err.message); });
 }
+
+function successOrFaiulre(res, successCallback, errorCallback) {
+    if (res.statusCode >= 400) {
+        console.error(res.statusCode);
+        console.error(res.body);
+        errorCallback(JSON.stringify(res.body));
+        return;
+    }
+
+    successCallback();
+    return;
+}
+
 let client = {
     activateRepo: function (fullName, callback, errorCallback) {
         refreshRepos(
             () => request.post(url.resolve(DRONE_URL, `/api/repos/${fullName}`))
                 .auth(bearer = DRONE_TOKEN)
-                .on('response', (resp) => { callback(); })
+                .on('response', (resp) => { successOrFaiulre(resp, callback, errorCallback); })
                 .on('error', (eResp) => errorCallback(eResp.message)),
             (error) => errorCallback(error)
         );
@@ -22,7 +37,7 @@ let client = {
         refreshRepos(
             () => request.delete(url.resolve(DRONE_URL, `/api/repos/${fullName}`))
                 .auth(bearer = DRONE_TOKEN)
-                .on('response', (resp) => { callback(); })
+                .on('response', (resp) => { successOrFaiulre(resp, callback, errorCallback); })
                 .on('error', (eResp) => errorCallback(eResp.message)),
             (error) => errorCallback(error)
         );
